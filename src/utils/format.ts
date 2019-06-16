f39d61ae0baf10a1bb06c158fca98c6021f26c11
jerenurminen <jere.m.nurminen@outlook.com>
2019-06-16 14:38:17 +0200

    Add naive line wrapping for function descriptions

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -3,17 +3,35 @@ enum Styles {
   Bold = '\x1b[1m'
 }
 
+const DEFAULT_WIDTH = 80
+
 const wrap = (style: Styles, text: string) => `${style}${text}\x1b[0m`
 
 const underline = (text: string): string => wrap(Styles.Underline, text)
 const bold = (text: string): string => wrap(Styles.Bold, text)
+
+const makeRow = (
+  width: number,
+  words: ReadonlyArray<string>,
+  currentRow: string = '',
+  rows: ReadonlyArray<string> = ['']
+): ReadonlyArray<string> =>
+  !words.length
+    ? [...rows, currentRow]
+    : currentRow.length + words[0].length >= width
+      ? makeRow(width, words.slice(1), words[0], [...rows, currentRow]) 
+      : makeRow(width, words.slice(1), `${currentRow} ${words[0]}`, rows)
+
+const formatDescription = (desc: string) =>
+  makeRow(process.stdout.columns || DEFAULT_WIDTH, desc.replace(/\n/g, ' ').split(' '))
 
 export const formatEntry = (entry: Entry) => [
   '',
   underline(bold(entry.name)),
   entry.signature,
   '',
-  ...entry.description.split('\n'),
+  ...formatDescription(entry.description),
+  '',
   ...entry.snippet.split('\n'),
   '',
   'See also:',
