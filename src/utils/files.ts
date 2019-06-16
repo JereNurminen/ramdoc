e21ed3af9b65d51f8ecfb5a2b082dde3e05e78a8
jerenurminen <jere.m.nurminen@outlook.com>
2019-06-16 15:37:25 +0200

    Persist docs in build dir

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,15 +1,17 @@
 import * as fs from 'fs'
 import { promisify } from 'util'
 
+const FILE_PATH = `${__dirname}/../docs.json`
+
 const read = promisify(fs.readFile)
 const write = promisify(fs.writeFile)
 
 export const writeLocalCopy = async (
   documentation: Documentation
-): Promise<void> => await write('docs', JSON.stringify(documentation))
+): Promise<void> => await write(FILE_PATH, JSON.stringify(documentation))
 
 export const checkLocalCopyExistence = async (): Promise<boolean> =>
-  new Promise(resolve => fs.access('docs', fs.constants.F_OK, e => resolve(!e)))
+  new Promise(resolve => fs.access(FILE_PATH, fs.constants.F_OK, e => resolve(!e)))
 
 export const readLocalCopy = async (): Promise<Documentation> => {
   process.stdout.write('Reading local copy of documentation...')
